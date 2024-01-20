290799936e09aa89ef6c868eee294e08589953f6
Abhay Bhardwaj <abhaybhardwaj0008@gmail.com>
2024-01-20 15:04:04 +0530

    dependencies changes

--- src/config/api.js
+++ src/config/api.js
@@ -1,26 +1,26 @@
 const API_KEY = process.env.REACT_APP_api_key
-// export const CoinList = (currency) =>
-//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${process.env.REACT_APP_api_key}`;
-
-// export const SingleCoin = (id) =>
-//   `https://api.coingecko.com/api/v3/coins/${id}`;
-
-// export const HistoricalChart = (id, days = 365, currency) =>
-//   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
-
-// export const TrendingCoins = (currency) =>
-//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&x_cg_demo_api_key=${process.env.REACT_APP_api_key}`;
-
-// {Online Api key}
 export const CoinList = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&api_key=${API_KEY}`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${process.env.REACT_APP_api_key}`;
 
 export const SingleCoin = (id) =>
-  `https://api.coingecko.com/api/v3/coins/${id}&api_key=${API_KEY}`;
+  `https://api.coingecko.com/api/v3/coins/${id}`;
 
 export const HistoricalChart = (id, days = 365, currency) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}&api_key=${API_KEY}`;
+  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
 
 export const TrendingCoins = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&api_key=${API_KEY}`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&x_cg_demo_api_key=${process.env.REACT_APP_api_key}`;
+
+// {Online Api key}
+// export const CoinList = (currency) =>
+//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&api_key=${API_KEY}`;
+
+// export const SingleCoin = (id) =>
+//   `https://api.coingecko.com/api/v3/coins/${id}&api_key=${API_KEY}`;
+
+// export const HistoricalChart = (id, days = 365, currency) =>
+//   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}&api_key=${API_KEY}`;
+
+// export const TrendingCoins = (currency) =>
+//   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h&api_key=${API_KEY}`;
 
